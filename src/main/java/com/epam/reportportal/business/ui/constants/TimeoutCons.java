e0e9f007a42faf886bb5785d0f77bd1c851c1d60
Alisa Harodnik <Alisa_Harodnik@epam.com>
2023-10-16 18:44:08 +0300

    Module #3: Test Automation Framework Design: update timeouts

--- src/main/java/com/epam/reportportal/business/ui/constants/TimeoutCons.java
+++ src/main/java/com/epam/reportportal/business/ui/constants/TimeoutCons.java
@@ -2,7 +2,7 @@ package com.epam.reportportal.business.ui.constants;
 
 public interface TimeoutCons {
 
-    int SHORT_TIMEOUT_IN_SECONDS = 2;
-    int MEDIUM_TIMEOUT_IN_SECONDS = 5;
-    int LONG_TIMEOUT_IN_SECONDS = 10;
+    int SHORT_TIMEOUT_IN_SECONDS = 5;
+    int MEDIUM_TIMEOUT_IN_SECONDS = 10;
+    int LONG_TIMEOUT_IN_SECONDS = 15;
 }
