4cc7f88f06ec2a0e886126e7cb156994a357c7bf
Alisa Harodnik <Alisa_Harodnik@epam.com>
2023-10-17 21:20:13 +0300

    Module #3: Test Automation Framework Design: update screen size

--- src/test/java/com/epam/reportportal/BaseTest.java
+++ src/test/java/com/epam/reportportal/BaseTest.java
@@ -3,6 +3,7 @@ package com.epam.reportportal;
 import com.epam.reportportal.core.common.utils.TestListener;
 import com.epam.reportportal.core.ui.drivers.DriverContainer;
 import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
@@ -18,7 +19,7 @@ public class BaseTest {
     @BeforeClass
     public void initTest(ITestContext context) {
         driver = DriverContainer.getDriver();
-        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1920, 1200));
         String variable = "driver";
         log.debug(String.format("Setting driver into context with variable name: %s", variable));
         context.setAttribute(variable, DriverContainer.getDriver());
